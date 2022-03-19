64b83749e48ee575adaa907a315338f9fc181b11
Noah Gao <noahgaocn@outlook.com>
2022-03-19 17:23:59 +0800

    feat: plugin meta

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: discourse-plugin-hide-signup
 # about: TODO
 # version: 0.0.1
-# authors: Discourse
+# authors: Ziheng Gao
 # url: TODO
 # required_version: 2.7.0
 # transpile_js: true
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :hide_signup_enabled
 
 after_initialize do
 end
